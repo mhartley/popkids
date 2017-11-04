00f68cc60fb60515af845ee6c3ed0f34279553a9
nikita ahuja <nahuja038@gmail.com>
2017-11-04 16:07:15 -0500

    Add eventbrite in requirements and work on completing methods in views

--- main/views.py
+++ main/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from eventbrite import Eventbrite
+import os
+import requests
+import json
+
 
 # Create your views here.
 
@@ -12,4 +17,38 @@ def sendgridpost(request):
 
 
 	return HttpResponse('we got a page.')
-		
+
+
+
+  token = os.environ.get('EVENTBRITE_OAUTH_PERSONAL')
+
+eventbrite = Eventbrite(token)
+user = eventbrite.get_user()
+
+
+def requests.get(
+    "https://www.eventbriteapi.com/v3/users/me/events/",
+    headers = {
+        "Authorization": "Bearer 5YJ4X6TACURDGGUKRTVE",
+    },
+    verify = True,  # Verify SSL certificate
+)
+
+return events.json()
+
+attendees = requests.get(
+  "https://www.eventbriteapi.com/v3/users/me/owned_event_attendees/",
+    headers = {
+        "Authorization": "Bearer 5YJ4X6TACURDGGUKRTVE",
+    },
+    verify = True,  # Verify SSL certificate
+  )
+
+attendees_object = attendees.json()['attendees']
+
+first_name = attendees_object[0]['profile']['first_name']
+last_name = attendees_object[0]['profile']['last_name']
+email = attendees_object[0]['profile']['email']
+attendance_status = attendees_object[0]['status']
+
+
