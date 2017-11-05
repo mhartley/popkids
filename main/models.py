040d008a5799271373289e3ae99e054e24f6d3c7
M H <morganhartley@gmail.com>
2017-11-05 12:00:29 -0600

    sendgrip app early integration

--- main/models.py
+++ main/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-
-from django.db import models
+from jsonfield import JSONField
+from django.utils import timezone
 
 class Person(models.Model):
 	first_name = models.CharField(max_length=30)
@@ -15,16 +15,21 @@ class Person(models.Model):
 	phone_number = models.CharField(max_length=30, null=True)
 	email = models.EmailField(null=True)
 	email2 = models.EmailField(null=True)
-	score = models.IntegerField()
+	score = models.IntegerField() 
 
 class Event(models.Model):
 	person = models.ForeignKey(Person)
 	event_type = models.CharField(max_length=30, null=True)
-	associate_id = models.CharField(max_length=100, null = True)
 	score_increment = models.IntegerField()
 	datetime = models.DateTimeField()
-	text = models.CharField(max_length=256)
-	node_id = models.CharField(max_length=30)
+	text = models.CharField(max_length=256, null = True)
+	node_id = models.CharField(max_length=30, null = True)
 
 class Postdata(models.Model):
 	textdump = models.TextField(null = True)
+
+class SendgridEvent(models.Model):
+    kind = models.CharField(max_length=75)
+    email = models.CharField(max_length=150)
+    data = JSONField(blank=True)
+    created_at = models.DateTimeField(default=timezone.now)
